7eaad12befa6673172ed24d2f3edb1d9559519e8
Reggie Piet <rjp52193@gmail.com>
2020-03-14 15:19:35 -0500

    Added CSS and non-required functions

---
+++ Module-2_Assessment/README.md
@@ -0,0 +1,12 @@
+# Module-2_Assessment
+This project is intended to demonstrate
+JavaScript's ability to dynamically create
+web pages by modifying the front end HTML
+and CSS. This particular example is a word
+guessing game that accepts keyboard input
+from a user and evaluates whether the
+letter typed is contained in the randomly
+chosen word. I decided to use a space theme
+for this project as there are many names
+for celestial objects that make ideal
+words for a guessing game such as this.
--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,18 +1,34 @@
 const wordList = ['Andromeda', 'Betelgeuse', 'Celestial',
-'Dipper', 'Eclipse', 'Ganymede', 'Hubble', 'Interstellar',
-'Jupiter', 'Oumuamua', 'Pluto', 'Satellite'];
-var curWord = wordList[Math.floor(Math.random()*12)];
-var guessArr = [];
+  'Dipper', 'Eclipse', 'Ganymede', 'Hubble', 'Interstellar',
+  'Jupiter', 'Oumuamua', 'Pluto', 'Satellite'];
+const clueList = [
+  'The closest neighboring galaxy of the Milky Way',
+  'A prominent red giant star in the constellation Orion',
+  'The word often used to describe objects in space',
+  'Nickname for two notable constellations, big and little',
+  'When the Earth or Moon fully obscures all sunlight',
+  'The largest moon in the solar system, orbits Jupiter',
+  'A legendary astronomer, also the name of a telescope',
+  'Being between stars, outside the solar system',
+  'A gas giant, the largest planet in our solar system',
+  'Name of a notable asteroid from outside the solar system',
+  'A dwarf planet that was once the ninth main planet',
+  'An object that orbits a planet, natural or manmade'];
 var message = document.querySelector('#message');
 var wins = document.querySelector('#wins');
-var guess = document.querySelector('#guess');
 var blanks = document.querySelector('#blanks');
+var guess = document.querySelector('#guess');
+var clue = document.querySelector('#clue');
 var guesses = document.querySelector('#guesses');
 var letters = document.querySelector('#letters');
+var guessArr = [];
 var updateStr = '';
 var winCnt = 0;
 var guessCnt = 15;
+var index = Math.floor(Math.random()*wordList.length);
+var curWord = wordList[index];
 wins.innerText = winCnt;
+clue.innerText = clueList[index];
 guesses.innerText = guessCnt;
 for(x=0;x<curWord.length;x++)
 {
@@ -33,7 +49,7 @@ function keyCheck(event)
   }
   if(guessCnt==0)
   {
-    message.innerText = 'You lose! Try again!';
+    message.innerText = 'Sorry! Try again!';
     gameReset();
   }
   if(curWord.toUpperCase().includes(key))
@@ -64,10 +80,13 @@ function letterStr(arr)
 }
 function gameReset()
 {
-  curWord = wordList[Math.floor(Math.random()*12)];
+  index = Math.floor(Math.random()*wordList.length);
+  curWord = wordList[index];
   guessArr = [];
   guessCnt = 15;
   blanks.innerText = blankStr(curWord, guessArr);
+  guess.innerText = '';
+  clue.innerText = clueList[index];
   guesses.innerText = guessCnt;
   letters.innerText = '';
 }
